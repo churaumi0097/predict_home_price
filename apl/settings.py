3f86a55f44bd48b0003eeb60977446cd38a9706b
Your Name <kamihuku@gmail.com>
2025-03-04 00:47:51 +0900

    New2

--- apl/settings.py
+++ apl/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = "apl.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+DATABASES = {'default': dj_database_url.config(default=default_dburl)}
 
 
 # Password validation
